d33f13c6b4707ee1c79b3e2d953f21b279091eb8
vagrant <vagrant@homestead>
2023-01-23 04:45:00 +0000

    Modified StripeContext in order to read .env file from frontend / installed dotenv and mix-env-file

--- resources/js/components/context/StripeContext.js
+++ resources/js/components/context/StripeContext.js
@@ -2,11 +2,9 @@ import React from 'react';
 import {loadStripe} from '@stripe/stripe-js';
 import {Elements} from '@stripe/react-stripe-js';
 
-// TODO: read .env file
-
 const StripeProvider = ({children}) => {
   // initialize stipe public key
-  const stripePromise = loadStripe('pk_test_51KsaLtCwHY0FaQM1CZfQqsBHTL9ozUN0xvyJFSZxNBn2tEMr1btdGm43cMvJ7CtN8yfOAWHDQVlvpjuRIf3RCoZt008wwWJgVr');
+  const stripePromise = loadStripe(process.env.MIX_STRIPE_KEY);
 
   return (
     // Stipe public key is passed to stripe object as props
